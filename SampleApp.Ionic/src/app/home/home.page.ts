46b9f8df857e60d7fd287e8a360fc78ac962a54e
artemov <artik3314@gmail.com>
2025-03-23 01:01:00 +0300

    Настройка приложения Flutter

--- SampleApp.Ionic/src/app/home/home.page.ts
+++ SampleApp.Ionic/src/app/home/home.page.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
